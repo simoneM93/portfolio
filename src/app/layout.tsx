d46eb3ef2e4b683d36f250083e5c4b60ebc0030e
Simone Marano <simonemarano93@gmail.com>
2026-01-26 10:04:00 +0100

    fix analytics bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,8 +35,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <PdfModalProvider />
         <Analytics/>
+        <PdfModalProvider />
       </body>
     </html>
   );
