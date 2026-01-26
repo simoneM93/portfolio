dcea3528fd69bdd8c3944b912f69ce1772275fda
Simone Marano <simonemarano93@gmail.com>
2026-01-26 09:54:13 +0100

    added analytics and deleted unused files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import PdfModalProvider from "@/components/PdfModalProvider";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,7 +35,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <PdfModalProvider />  {/* Sempre presente, invisibile se chiuso */}
+        <PdfModalProvider />
+        <Analytics/>
       </body>
     </html>
   );
